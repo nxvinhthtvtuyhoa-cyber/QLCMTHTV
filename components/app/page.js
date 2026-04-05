a90ff187221d4a01f58323b512bb187d0c788eb5
nxvinhthtvtuyhoa-cyber <nxvinh.th.tv.tuyhoa@daklak.edu.vn>
2026-04-05 15:14:44 +0700

    Add Home component with LoginButton

---
+++ components/app/page.js
@@ -0,0 +1,10 @@
+import LoginButton from "../components/LoginButton";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>QL Chuyên Môn Số</h1>
+      <LoginButton />
+    </main>
+  );
+}
