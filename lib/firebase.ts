a277d0a3b571eb5ed439455784abe7446023d435
nxvinhthtvtuyhoa-cyber <nxvinh.th.tv.tuyhoa@daklak.edu.vn>
2026-04-05 15:12:16 +0700

    Add authentication and storage to Firebase setup

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// lib/firebase.js
 
-// Firebase mới của bạn
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAf656sHIz2xihPbyY359kpEUUM3Vk8yOU",
   authDomain: "qlcmthtv-7766e.firebaseapp.com",
@@ -9,10 +12,12 @@ const firebaseConfig = {
   storageBucket: "qlcmthtv-7766e.firebasestorage.app",
   messagingSenderId: "60295783033",
   appId: "1:60295783033:web:85ab055b9d9eb738256aa8",
+  measurementId: "G-KH4SMKXVP8"
 };
 
-// kết nối firebase
 const app = initializeApp(firebaseConfig);
 
-// tạo database (Firestore)
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+export const storage = getStorage(app);
